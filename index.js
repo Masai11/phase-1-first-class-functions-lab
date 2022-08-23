faee84f827e3f2194255aa12dc15cb0ef78c47b2
Masai11 <roy.kiplagat@student.moringaschool.com>
2022-08-23 19:58:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+let drivers = ['Chach', 'Clifford', 'Phill', 'Nicky'];
+const returnFirstTwoDrivers = (drivers) => {
+    return [...drivers].slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = (drivers) => {
+    return [...drivers].slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = (fare) => {
+    return (val) => {
+      
+      return fare * val;
+    };
+  };
+  
+  let fareDoubler = createFareMultiplier(2);
+  let fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = (arrayOfDrivers, driversToReturn) => {
+    return driversToReturn(arrayOfDrivers);
+  };
